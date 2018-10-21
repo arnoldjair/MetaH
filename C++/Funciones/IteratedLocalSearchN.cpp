ef3068233c5e47f688e7941d3d716fc3b463b97e
Arnold jair Jimenez Vargas <ajjimenez@unicauca.edu.co>
2018-10-20 20:35:48 -0500

    Neighbours

---
+++ C++/Funciones/IteratedLocalSearchN.cpp
@@ -0,0 +1,63 @@
+/*
+ * IteratedLocalSearch.cpp
+ *
+ *  Created on: 12/10/2018
+ *      Author: Arnold Jair Jiménez Vargas <arnoldjair at gmail dot com>
+ */
+
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <thread>
+
+#include "IteratedLocalSearch.h"
+
+IteratedLocalSearch::IteratedLocalSearch() {
+}
+
+IteratedLocalSearch::~IteratedLocalSearch() {
+}
+
+Record* IteratedLocalSearch::process(int dimentionlity, int neighbours,
+                                     double lower, double upper, double tweak,
+                                     double perturbation, int funcEvaluations,
+                                     Function* function) {
+
+  std::unique_ptr<Record> s(
+      Record::randomRecord(dimentionlity, lower, upper, tweak));
+
+  s->setFitness(function->evaluate(*s));
+  std::unique_ptr<Record> h = std::make_unique<Record>();
+  std::unique_ptr<Record> best = std::make_unique<Record>();
+  std::unique_ptr<Record> r = std::make_unique<Record>();
+  h->copy(s.get());
+  best->copy(s.get());
+
+  bool cont = true;
+  int currNeighbours = 0;
+
+  do {
+    currNeighbours = 0;
+
+    do {
+      currNeighbours++;
+      s->tweak(r.get());
+      r->setFitness(function->evaluate(*r));
+      if (r->getFitness() < s->getFitness()) {
+        s->copy(r.get());
+      }
+    } while (currNeighbours < neighbours);
+
+    if (s->getFitness() < best->getFitness()) {
+      best->copy(s.get());
+    }
+
+    if (s->getFitness() < h->getFitness()) {
+      h->copy(s.get());
+    }
+
+    h->perturb(perturbation, s.get());
+    s->setFitness(function->evaluate(*s));
+  } while (function->getEvaluationCount() < funcEvaluations);
+  return best.get();
+}
---
+++ C++/Funciones/IteratedLocalSearchN.h
@@ -0,0 +1,27 @@
+/*
+ * IteratedLocalSearch.h
+ *
+ *  Created on: 12/10/2018
+ *      Author: Arnold Jair Jiménez Vargas <arnoldjair at gmail dot com>
+ */
+
+#ifndef ITERATEDLOCALSEARCH_H_
+#define ITERATEDLOCALSEARCH_H_
+
+#include "Function.h"
+#include "Record.h"
+class IteratedLocalSearch {
+ public:
+  IteratedLocalSearch();
+  virtual ~IteratedLocalSearch();
+
+  /**
+   * Time in miliseconds
+   */
+  Record* process(int dimentionality, int neighbours, double lower,
+                  double upper, double tweak, double perturbation,
+                  int funcEvaluations, Function* function);
+
+};
+
+#endif /* ITERATEDLOCALSEARCH_H_ */
